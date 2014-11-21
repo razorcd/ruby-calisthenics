934ee0db752296d6d61fc4a8ff9697189c732bfe
saasbook <saasbook@saasbook.(none)>
2014-11-21 12:31:36 -0400

    fun_with_strings_

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -20,17 +20,18 @@ module FunWithStrings
     words_array = self.downcase.split(/[^a-z]/i)
     words_array.delete('')
     new_words_array = []
-    words_array[0..-2].each_with_index do |v,i|
-      words_array[i+1..-1].each do |v2|
-        puts "Checking: #{v} and #{v2}"
-        new_words_array << [v,v2] if v == v2[-1]+v2[0..-2]
-        new_words_array << [v,v2] if v2 == v[-1]+v[0..-2]
-      end
-      puts "******************"
-      puts words_array.inspect
-      puts new_words_array.inspect
-      new_words_array
+    
+    #checking for anagrams
+    words_array.permutation(2).each do |pa|
+      new_words_array << pa.reverse if pa[0] == pa[1][-1] + pa[1][0..-2]
     end
+
+    #adding non anagram words
+    words_array.each do |w|
+      new_words_array << [w] if new_words_array.flatten.all? {|t| t!=w}
+    end
+
+    new_words_array
   end
 end
 
