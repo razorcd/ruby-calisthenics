f5619fb8b121bed2106f08098b8b88ebd42f96ba
saasbook <saasbook@saasbook.(none)>
2014-11-21 11:39:28 -0400

    array

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -6,9 +6,31 @@ module FunWithStrings
   end
   def count_words
     # your code here
+    words_array = self.downcase.split(/[^a-z]/i)
+    words_array.delete('')
+    words_hash = {}
+    words_array.uniq.each do |k|
+      words_hash[k] = words_array.count(k)
+    end
+    words_hash
   end
   def anagram_groups
     # your code here
+    return [] if self.empty?
+    words_array = self.downcase.split(/[^a-z]/i)
+    words_array.delete('')
+    new_words_array = []
+    words_array[0..-2].each_with_index do |v,i|
+      words_array[i+1..-1].each do |v2|
+        puts "Checking: #{v} and #{v2}"
+        new_words_array << [v,v2] if v == v2[-1]+v2[0..-2]
+        new_words_array << [v,v2] if v2 == v[-1]+v[0..-2]
+      end
+      puts "******************"
+      puts words_array.inspect
+      puts new_words_array.inspect
+      new_words_array
+    end
   end
 end
 
